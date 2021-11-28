cb823cf48b4a474713f5714f8f5df01993ccf562
TextError <savinraul1983@yahoo.com>
2021-11-28 12:36:08 +0000

    More to form validation

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -66,7 +66,7 @@
             </button>
           </form>
           <!-- Registration Form -->
-          <VeeForm v-show="!tab" :validation-schema="schema" @submit="onSubmit">
+          <VeeForm v-show="!tab" :validation-schema="schema" :initial-values="values" @submit="onSubmit">
             <!-- Name -->
             <div class="mb-3">
               <label class="inline-block mb-2">Name</label>
@@ -105,14 +105,16 @@
             <!-- Password -->
             <div class="mb-3">
               <label class="inline-block mb-2">Password</label>
-              <VeeField
-                name="password"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
+              <VeeField name="password" :bails="false" v-slot="{ field, errors }">
+                <input 
+                  class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
                   duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Password" 
-                type="password"
-              />
-              <ErrorMessage class="text-red-600" name='password' />
+                  placeholder="Password" 
+                  type="password"
+                  v-bind="field"
+                />
+                <div class="text-red-600" v-for="error in errors" :key="error">{{ error }}</div>
+              </VeeField>
             </div>
             <!-- Confirm Password -->
             <div class="mb-3">
@@ -173,8 +175,11 @@ import { mapState, mapMutations } from 'vuex';
           age: 'required|min_value:18|max_value:100',
           password: 'required|min:8|max:100',
           confirm_password: 'confirmed:@password',
-          country: 'required|excluded:Antartica',
-          tos: 'required'
+          country: 'required|country_excluded:Antartica',
+          tos: 'tos'
+        },
+        values: {
+          country: 'USA'
         }
       };
     },
--- src/includes/validation.js
+++ src/includes/validation.js
@@ -1,5 +1,5 @@
-import { Form as VeeForm, Field as VeeField, defineRule, ErrorMessage } from 'vee-validate';
-import { required, min, max, alpha_spaces as alphaSpaces, email, min_value as minValue, max_value as maxValue, confirmed, not_one_of as notOneOf } from '@vee-validate/rules';
+import { Form as VeeForm, Field as VeeField, defineRule, ErrorMessage, configure } from 'vee-validate';
+import { required, min, max, alpha_spaces as alphaSpaces, email, min_value as minValue, max_value as maxValue, confirmed, not_one_of as excluded } from '@vee-validate/rules';
 
 export default {
   install(app) {
@@ -8,6 +8,7 @@ export default {
     app.component('ErrorMessage', ErrorMessage);
 
     defineRule('required', required);
+    defineRule('tos', required);
     defineRule('min', min);
     defineRule('max', max);
     defineRule('alpha_spaces', alphaSpaces);
@@ -15,6 +16,31 @@ export default {
     defineRule('min_value', minValue);
     defineRule('max_value', maxValue);
     defineRule('confirmed', confirmed);
-    defineRule('excluded', notOneOf);
+    defineRule('excluded', excluded);
+    defineRule('country_excluded', excluded);
+
+    configure({
+      generateMessage: (ctx) => {
+        const messages = {
+          required: `The ${ctx.field} field is required!`,
+          tos: 'You must accept the Term of Service',
+          min: `The ${ctx.field} field is to short!`,
+          max: `The ${ctx.field} field is to long!`,
+          alpha_spaces: `The ${ctx.field} field may only contain alphabetic characters and spaces!`,
+          email: `The ${ctx.field} field must be a valid email!`,
+          min_value: `The ${ctx.field} field is to low!`,
+          max_value: `The ${ctx.field} field is to heigh!`,
+          confirmed: 'The passwords do not match!',
+          excluded: `You are not allowed to use this value for the field ${ctx.field}!`,
+          country_excluded: 'Due to restriction, we do not accept users from this location!'
+        };
+        const message = messages[ctx.rule.name] ? messages[ctx.rule.name] : `The field ${ctx.field} is invalid!`;
+        return message;
+      },
+      validateOnBlur: false,
+      validateOnChange: true,
+      validateOnInput: false,
+      validateOnModelUpdate: true
+    });
   }
 };
