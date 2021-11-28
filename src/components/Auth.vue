03253edbbe09b19d3e368661c5dc6001f28feb28
TextError <savinraul1983@yahoo.com>
2021-11-28 11:56:28 +0000

    Form validation

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -26,18 +26,23 @@
 
           <!-- Tabs -->
           <ul class="flex flex-wrap mb-4">
-            <li class="flex-auto text-center">
-              <a class="block rounded py-3 px-4 transition hover:text-white text-white
-                bg-blue-600" href="#">Login</a>
+            <li class="flex-auto text-center" @click.prevent="tab = !tab">
+              <a class="block rounded py-3 px-4 transition" :class="{
+                'hover:text-white text-white bg-blue-600' : tab,
+                'hover:text-blue' : !tab
+              }" href="#">Login</a>
             </li>
-            <li class="flex-auto text-center">
-              <a class="block rounded py-3 px-4 transition" 
+            <li class="flex-auto text-center" @click.prevent="tab = !tab">
+              <a class="block rounded py-3 px-4 transition" :class="{
+                'hover:text-white text-white bg-blue-600' : !tab,
+                'hover:text-blue' : tab
+              }" 
                 href="#">Register</a>
             </li>
           </ul>
 
           <!-- Login Form -->
-          <form>
+          <form v-show="tab">
             <!-- Email -->
             <div class="mb-3">
               <label class="inline-block mb-2">Email</label>
@@ -61,68 +66,93 @@
             </button>
           </form>
           <!-- Registration Form -->
-          <form>
+          <VeeForm v-show="!tab" :validation-schema="schema" @submit="onSubmit">
             <!-- Name -->
             <div class="mb-3">
               <label class="inline-block mb-2">Name</label>
-              <input type="text"
+              <VeeField 
+                name="name" 
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
                   duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Enter Name" />
+                placeholder="Enter Name" 
+                type="text"
+              />
+              <ErrorMessage class="text-red-600" name='name' />
             </div>
             <!-- Email -->
             <div class="mb-3">
               <label class="inline-block mb-2">Email</label>
-              <input type="email"
+              <VeeField
+                name="email" 
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
                   duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Enter Email" />
+                placeholder="Enter Email" 
+                type="email"
+              />
+              <ErrorMessage class="text-red-600" name='email' />
             </div>
             <!-- Age -->
             <div class="mb-3">
               <label class="inline-block mb-2">Age</label>
-              <input type="number"
+              <VeeField
+                name='age'
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
-                  duration-500 focus:outline-none focus:border-black rounded" />
+                  duration-500 focus:outline-none focus:border-black rounded" 
+                type="number"
+                />
+              <ErrorMessage class="text-red-600" name='age' />
             </div>
             <!-- Password -->
             <div class="mb-3">
               <label class="inline-block mb-2">Password</label>
-              <input type="password"
+              <VeeField
+                name="password"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
                   duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Password" />
+                placeholder="Password" 
+                type="password"
+              />
+              <ErrorMessage class="text-red-600" name='password' />
             </div>
             <!-- Confirm Password -->
             <div class="mb-3">
               <label class="inline-block mb-2">Confirm Password</label>
-              <input type="password"
+              <VeeField
+                name="confirm-password"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
                   duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Confirm Password" />
+                placeholder="Confirm Password"
+                type="password"
+                />
+                <ErrorMessage class="text-red-600" name='confirm_passowrd' />
             </div>
             <!-- Country -->
             <div class="mb-3">
               <label class="inline-block mb-2">Country</label>
-              <select
+              <VeeField 
+                as="select"
+                name="country"
                 class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
                   duration-500 focus:outline-none focus:border-black rounded">
                 <option value="USA">USA</option>
                 <option value="Mexico">Mexico</option>
                 <option value="Germany">Germany</option>
-              </select>
+                <option value="Antartica">Antartica</option>
+              </VeeField>
+              <ErrorMessage class="text-red-600" name='country' />
             </div>
             <!-- TOS -->
             <div class="mb-3 pl-6">
-              <input type="checkbox" class="w-4 h-4 float-left -ml-6 mt-1 rounded" />
+              <VeeField name="tos" type="checkbox" class="w-4 h-4 float-left -ml-6 mt-1 rounded" value="1" />
               <label class="inline-block">Accept terms of service</label>
+              <ErrorMessage class="text-red-600 block" name='tos' />
             </div>
             <button type="submit"
               class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition
                 hover:bg-purple-700">
               Submit
             </button>
-          </form>
+          </VeeForm>
         </div>
       </div>
     </div>
@@ -134,6 +164,20 @@ import { mapState, mapMutations } from 'vuex';
 
   export default {
     name: 'Auth',
+    data() {
+      return {
+        tab: true,
+        schema: {
+          name: 'required|min:2|max:100|alpha_spaces',
+          email: 'required|min:2|max:100|email',
+          age: 'required|min_value:18|max_value:100',
+          password: 'required|min:8|max:100',
+          confirm_password: 'confirmed:@password',
+          country: 'required|excluded:Antartica',
+          tos: 'required'
+        }
+      };
+    },
     methods: {
       ...mapMutations({ onToggle: 'toggleAuthModal' })
     },
--- src/main.js
+++ src/main.js
@@ -2,13 +2,21 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import VeeValidatePlugin from './includes/validation';
+// Css
 import './assets/tailwind.css';
 import './assets/main.css';
 
 const app = createApp(App);
 
+// Vuex
 app.use(store);
 
+// Router
 app.use(router);
 
+// Plugins
+app.use(VeeValidatePlugin);
+
+// App
 app.mount('#app');
